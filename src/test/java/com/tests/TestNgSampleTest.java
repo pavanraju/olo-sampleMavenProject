be85cc5e579cbaf52be9dc13e2811719feb5f55f
Pavan Kumar Raju <pavan.mailme@gmail.com>
2014-02-10 12:09:33 +0530

    updated sample project with latest olo version

--- src/test/java/com/tests/TestNgSampleTest.java
+++ src/test/java/com/tests/TestNgSampleTest.java
@@ -1,7 +1,10 @@
 package com.tests;
 
+import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
+
+import com.olo.Verify;
 
 public class TestNgSampleTest {
 	
@@ -12,12 +15,24 @@ public class TestNgSampleTest {
 	
 	@Test
 	public void test2(){
-		
+		Assert.assertEquals("test","test");
 	}
 	
 	@Test
 	public void test3() throws Exception{
-		//throw new Exception("Failure");
+		throw new Exception("Failure");
+	}
+	
+	@Test
+	public void test4(){
+		Verify.verifyEquals("test", "test1");
+		Verify.verifyEquals(true, false);
+	}
+	
+	@Test
+	public void test5(){
+		Verify.verifyEquals("test", "test");
+		Verify.verifyEquals(true, true);
 	}
 	
 }
--- src/test/java/com/tests/WebDriverPageObjectModelTest.java
+++ src/test/java/com/tests/WebDriverPageObjectModelTest.java
@@ -3,15 +3,23 @@ package com.tests;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import com.olo.Verify;
 import com.olo.initiator.ApplicationInitiator;
 import com.pages.LoginPage;
 
 public class WebDriverPageObjectModelTest extends ApplicationInitiator{
 	
 	@Test
-	public void loginTest() throws Exception{
+	public void loginTest () throws Exception{
 		LoginPage lp = PageFactory.initElements(getDriver(), LoginPage.class);
 		lp.login("asdfasdf", "asdfasdf");
+	}
+	
+	@Test
+	public void verificationTest() throws Exception{
+		LoginPage lp = PageFactory.initElements(getDriver(), LoginPage.class);
+		lp.login("asdfasdf", "asdfasdf");
+		Verify.verifyEquals(getDriver().getTitle(), "Twitter", getDriver());
 	}
 
 }
